9b1f3c5cb0850631353104d0714c2c656587bcce
Oleh <orenkole@gmail.com>
2021-04-27 19:18:13 +0300

    Updates

---
+++ precache-manifest.0b0c34bf04fba9412fd608369f076b40.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8dff70008083ce6d9a6e50d63cd8637",
+    "url": "/it-kamasutra-react/index.html"
+  },
+  {
+    "revision": "ac1f5d4adae98d25223e",
+    "url": "/it-kamasutra-react/static/css/3.90f6fe71.chunk.css"
+  },
+  {
+    "revision": "51a5d089140b2e93660e",
+    "url": "/it-kamasutra-react/static/css/4.a42d9650.chunk.css"
+  },
+  {
+    "revision": "1ad343abdfca5eaa11ca",
+    "url": "/it-kamasutra-react/static/css/main.63da198d.chunk.css"
+  },
+  {
+    "revision": "0201a90d95414b18b663",
+    "url": "/it-kamasutra-react/static/js/2.3e984284.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/it-kamasutra-react/static/js/2.3e984284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac1f5d4adae98d25223e",
+    "url": "/it-kamasutra-react/static/js/3.57d0be29.chunk.js"
+  },
+  {
+    "revision": "51a5d089140b2e93660e",
+    "url": "/it-kamasutra-react/static/js/4.5016f08c.chunk.js"
+  },
+  {
+    "revision": "1ad343abdfca5eaa11ca",
+    "url": "/it-kamasutra-react/static/js/main.98b97036.chunk.js"
+  },
+  {
+    "revision": "db553182664129c7f282",
+    "url": "/it-kamasutra-react/static/js/runtime-main.67ec4361.js"
+  },
+  {
+    "revision": "e70633c68c9a815e5fd1b4dfd988ed71",
+    "url": "/it-kamasutra-react/static/media/808.e70633c6.gif"
+  }
+]);
